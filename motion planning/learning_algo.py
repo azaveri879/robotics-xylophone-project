ada05761befd029dd4272ecc093e5e2dbe8c674b
azaveri879 <zaveri.a@northeastern.edu>
2024-03-11 15:44:34 -0400

    Initial commit/files for motion planning

---
+++ motion planning/learning_algo.py
@@ -0,0 +1,9 @@
+"""
+Function to learn how to play the xylophone using a reinforcement learning/graph building algorithm.
+
+Input:
+- Sound/listener data
+
+Output:
+- List of pairs of keys where the first key is for left mallet and the second key is for right mallet
+"""
---
+++ motion planning/mallet_decision.py
@@ -0,0 +1,8 @@
+"""
+Function to decide which mallet to use for each key in the song.
+Input: 
+- List of notes
+- Map of pairs of keys to distance
+Output:
+- List of pairs of keys where the first key is for left mallet and the second key is for right mallet
+"""
