c923acc48fd96820d62c2300e3f760a9521eca14
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2024-04-16 19:34:00 -0400

    Adding note reading

--- robot/XyloBot.py
+++ robot/XyloBot.py
@@ -22,12 +22,23 @@ class XyloBot():
 		self.last_wrist_pos = 0
 		
 		self.m = {}
-		notes = ["G", "A", "B", "C", "D", "E", "F"]
+		
+		notes = ["G1", "A1", "B1", "C1", "D1"]
 		for i, note in enumerate(notes):
-			self.m[note] = (0.028 + 0.032*i, 0.45)
+			self.m[note] = (-0.17 + 0.03*i, 0.45)
 			
-		self.left_mallet = (-0.02, 0.2)
-		self.right_mallet = (0.02, 0.2)
+		notes = ["E1", "F1", "G2", "A2", "B2"]
+		for i, note in enumerate(notes):
+			self.m[note] = (-0.031 + 0.03*i, 0.45)
+			
+		notes = ["C2", "D2", "E2", "F2", "G3"]
+		for i, note in enumerate(notes):
+			self.m[note] = (0.105 + 0.03*i, 0.45)
+			
+		self.left_mallet = (-0.068, 0.2)
+		self.right_mallet = (0.068, 0.2)
+		self.mallet_distance = 0
+		self.left = True
 
 	def find_ee_pose(
         self,
@@ -75,7 +86,7 @@ class XyloBot():
 	def go_to(self,
         x: float = 0,
         y: float = 0,
-        z: float = 0, left: bool = True):
+        z: float = 0):
 
 		thetas, success = self.find_ee_pose(x=x, y=y, z=z)
 		if not success:
@@ -84,9 +95,28 @@ class XyloBot():
 		thetas[3] -= 0.2
 		self.last_wrist_pos = thetas[3]
 		
-		thetas[4] = -0.22 if left else 0.22
+		rotation = self.get_wrist_rotation()
+		thetas[4] = -rotation if self.left else rotation
 		
 		return self.bot.arm.set_joint_positions(thetas, moving_time=1.0)
+	
+	def get_wrist_rotation(self):
+		goal_height_difference = 0.025
+		radius = self.mallet_distance / 2
+		return math.atan(goal_height_difference/radius)
+	
+	def find_mallet_location(self):
+		mallet = self.left_mallet if self.left else self.right_mallet
+		rotation = self.get_wrist_rotation()
+		print(rotation)
+		print(self.mallet_distance)
+		x_pos = (self.mallet_distance/2)*math.cos(rotation)
+		print(x_pos)
+		if self.left:
+			return (-x_pos, mallet[1])
+		else:
+			return (x_pos, mallet[1])
+        
 
 	def hit(self):
 		self.bot.arm.set_single_joint_position("wrist_angle", self.last_wrist_pos + 0.25, moving_time=1.0)
@@ -97,10 +127,19 @@ class XyloBot():
 		
 	def play_song(self, notes):
 		for n, left in notes:
-			mallet = self.left_mallet if left else self.right_mallet
-			pos = self.find_ee_loc(self.m[n], mallet)
-			self.go_to(x=pos[0], y=pos[1], z=0.15, left=left)
+			self.left = left
+			self.mallet_distance = 0.136
+			mallet_location = self.find_mallet_location()
+			ee_loc = self.find_ee_loc(self.m[n], mallet_location)
+			self.go_to(x=ee_loc[0], y=ee_loc[1], z=0.15)
 			self.hit()
+			
+	def gripper_to_distance(self, angle):
+		self.bot.core.robot_set_operating_modes(cmd_type='single', name='gripper', mode='position')
+		self.wait(0.5)
+		self.bot.core.robot_write_joint_command(joint_name="gripper", command=angle)
+		self.wait(0.5)
+		self.bot.core.robot_set_operating_modes(cmd_type='single', name='gripper', mode='pwm')
 
 	def go_home(self):
 		self.bot.arm.set_joint_positions([rads(90), rads(-105), 1.55, rads(20), 0.0], moving_time=1.0)
--- robot/test.py
+++ robot/test.py
@@ -1,4 +1,5 @@
 from XyloBot import XyloBot
+import numpy as np
 
 def main():
 	notes = ["G", "A", "B", "C", "D"]
@@ -10,9 +11,8 @@ def main():
 	right_mallet = (0.02, 0.2)
 
 	xylo = XyloBot()
-	xylo.bot.gripper.release()
 	# pos = xylo.find_ee_loc(m["C"], right_mallet)
-	# xylo.go_to(x=pos[0], y=pos[1], z=0.15, left=False)
+	# xylo.go_to(x=pos[0], y=pos[1], z=0.15, left=True)
 	# xylo.hit()
 	# xylo.hit()
 	# # xylo.wait(3)
@@ -28,8 +28,14 @@ def main():
 	# pos = xylo.find_ee_loc(m["G"], left_mallet)
 	# xylo.go_to(x=pos[0], y=pos[1], z=0.15)
 	# xylo.hit()
-	xylo.play_song([("C", False), ("C", False), ("G", True), ("G", True), ("A", False), ("A", False), ("G", True), ("F", True)])
+	xylo.gripper_to_distance(np.pi/4)
+	xylo.wait(1)
+	# xylo.play_song([("G1", True), ("A1", True), ("B1", True), ("C1", True), ("D1", True), ("E1", True), ("F1", True), ("G2", True), ("A2", True), ("B2", True), ("C2", True), ("D2", True), ("E2", True), ("F2", True), ("G3", True)])
+	xylo.play_song([("C1", True), ("D1", True), ("E1", False)])
 	xylo.go_home()
+	# xylo.gripper_to_distance(0.0)
+	# xylo.wait(1)
+	# xylo.gripper_to_distance(np.pi/2)
 	xylo.shutdown()
 
 if __name__ == '__main__':
