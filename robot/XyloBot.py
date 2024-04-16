7869ab91611f4bec5fb197e5f8483b9b1a128e33
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2024-04-16 18:12:54 -0400

    Adding play_song

--- robot/XyloBot.py
+++ robot/XyloBot.py
@@ -19,6 +19,15 @@ class XyloBot():
 			group_name='arm',
 			gripper_name='gripper'
 		)
+		self.last_wrist_pos = 0
+		
+		self.m = {}
+		notes = ["G", "A", "B", "C", "D", "E", "F"]
+		for i, note in enumerate(notes):
+			self.m[note] = (0.028 + 0.032*i, 0.45)
+			
+		self.left_mallet = (-0.02, 0.2)
+		self.right_mallet = (0.02, 0.2)
 
 	def find_ee_pose(
         self,
@@ -26,49 +35,75 @@ class XyloBot():
         y: float = 0,
         z: float = 0
     ) -> Tuple[Union[np.ndarray, Any, List[float]], bool]:
-		return self.bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=-0.2, execute=False)
+		return self.bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0, execute=False)
 	
 	def find_ee_loc(self, note_location: Tuple[float, float], mallet_location_ee: Tuple[float, float]):
+		
+        # Find the length of the mallet and the note
 		mallet_length = math.sqrt(mallet_location_ee[0]**2 + mallet_location_ee[1]**2)
-		print("lm", mallet_length)
+		# print("mallet_length", mallet_length)
 		note_length = math.sqrt(note_location[0]**2 + note_location[1]**2)
-		print("ln", note_length)
+		# print("note_length", note_length)
 
 
-		note_angle = math.atan(note_location[1]/note_location[0])
-		print("theta_1", deg(note_angle))
+        # Find the angles of the mallets and notes
+		note_angle = math.atan(note_location[0]/note_location[1])
+		# print("note_angle", deg(note_angle))
 		mallet_angle = np.pi/2 + np.abs(math.atan(mallet_location_ee[1]/mallet_location_ee[0]))
-		print("theta_2", deg(mallet_angle))
+		# print("mallet_angle", deg(mallet_angle))
 		bottom_angle = math.asin(math.sin(mallet_angle)*mallet_length/note_length)
-		print("theta_4", deg(bottom_angle))
+		# print("bottom_angle", deg(bottom_angle))
 
+        # Find angle of the arm
 		if mallet_location_ee[0]*note_location[0] > 0: # Both are negative or positive
+			# print("Using same side")
 			arm_angle = (np.abs(note_angle) - bottom_angle)*note_angle/np.abs(note_angle)
 		else:
+			# print("Using left and right")
 			arm_angle = (np.abs(note_angle) + bottom_angle)*note_angle/np.abs(note_angle)
+		# print("arm_angle", deg(arm_angle))
+		
+        # Find final angle in triangle
+		last_triangle_angle = np.pi - bottom_angle - mallet_angle
+		# print("last_triangle_angle", deg(last_triangle_angle))
 
-		print("theta_3", deg(arm_angle))
-
-		arm_length = math.sqrt(mallet_length**2 + note_length**2 - 2*mallet_length*note_length*math.cos(np.abs(arm_angle)))
-		print("la", arm_length)
+        # Find arm length and final ee position
+		arm_length = math.sqrt(mallet_length**2 + note_length**2 - 2*mallet_length*note_length*math.cos(np.abs(last_triangle_angle)))
+		# print("arm_length", arm_length)
 		return (math.sin(arm_angle)*arm_length, math.cos(arm_angle)*arm_length)
 	
 	def go_to(self,
         x: float = 0,
         y: float = 0,
-        z: float = 0):
+        z: float = 0, left: bool = True):
 
 		thetas, success = self.find_ee_pose(x=x, y=y, z=z)
 		if not success:
 			print("Couldn't find pose")
 			return False
-		return self.bot.arm.set_joint_positions(thetas, moving_time=3.0)
+		thetas[3] -= 0.2
+		self.last_wrist_pos = thetas[3]
+		
+		thetas[4] = -0.22 if left else 0.22
+		
+		return self.bot.arm.set_joint_positions(thetas, moving_time=1.0)
+
+	def hit(self):
+		self.bot.arm.set_single_joint_position("wrist_angle", self.last_wrist_pos + 0.25, moving_time=1.0)
+		self.bot.arm.set_single_joint_position("wrist_angle", self.last_wrist_pos, moving_time=1.0)
 	
 	def wait(self, secs):
 		time.sleep(secs)
+		
+	def play_song(self, notes):
+		for n, left in notes:
+			mallet = self.left_mallet if left else self.right_mallet
+			pos = self.find_ee_loc(self.m[n], mallet)
+			self.go_to(x=pos[0], y=pos[1], z=0.15, left=left)
+			self.hit()
 
 	def go_home(self):
-		self.bot.arm.set_joint_positions([rads(90), rads(-105), 1.55, rads(20), 0.0], moving_time=3.0)
+		self.bot.arm.set_joint_positions([rads(90), rads(-105), 1.55, rads(20), 0.0], moving_time=1.0)
 
 	def shutdown(self):
 		self.bot.shutdown()
--- robot/test.py
+++ robot/test.py
@@ -1,16 +1,35 @@
-from robot.XyloBot import XyloBot
-
+from XyloBot import XyloBot
 
 def main():
-	xylo = XyloBot()
+	notes = ["G", "A", "B", "C", "D"]
+	m = {}
+	for i, note in enumerate(notes):
+		m[note] = (0.028 + 0.03*i, 0.45)
+		
+	left_mallet = (-0.02, 0.2)
+	right_mallet = (0.02, 0.2)
 
-	# print(xylo.bot.arm.get_ee_pose())
-	# xylo.go_to(x=0.05, y=0.22647966, z=0.15)
-	# xylo.bot.gripper.grasp()
-	# xylo.bot.gripper.release()
-	# xylo.wait(1)
-	# xylo.go_home()
-	print(xylo.find_ee_loc((-3, 3), (1, 1)))
+	xylo = XyloBot()
+	xylo.bot.gripper.release()
+	# pos = xylo.find_ee_loc(m["C"], right_mallet)
+	# xylo.go_to(x=pos[0], y=pos[1], z=0.15, left=False)
+	# xylo.hit()
+	# xylo.hit()
+	# # xylo.wait(3)
+	# pos = xylo.find_ee_loc(m["G"], left_mallet)
+	# xylo.go_to(x=pos[0], y=pos[1], z=0.15)
+	# xylo.hit()
+	# xylo.hit()
+	# # xylo.wait(3)
+	# pos = xylo.find_ee_loc(m["A"], left_mallet)
+	# xylo.go_to(x=pos[0], y=pos[1], z=0.15)
+	# xylo.hit()
+	# xylo.hit()
+	# pos = xylo.find_ee_loc(m["G"], left_mallet)
+	# xylo.go_to(x=pos[0], y=pos[1], z=0.15)
+	# xylo.hit()
+	xylo.play_song([("C", False), ("C", False), ("G", True), ("G", True), ("A", False), ("A", False), ("G", True), ("F", True)])
+	xylo.go_home()
 	xylo.shutdown()
 
 if __name__ == '__main__':
